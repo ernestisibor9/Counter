cf50507c69f2af71058b98a5320a9908ec384d91
Ernest <ernestisibor9@gmail.com>
2023-06-06 11:03:49 +0100

    Event Delegation

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
+// Event Delegation
+
 const counter = document.getElementById("counter");
+const buttons = document.querySelector(".buttons");
 
-const myAdd = document.querySelector(".add");
-const myReset = document.querySelector(".reset");
-const mySub = document.querySelector(".sub");
+// Add event listeners
+buttons.addEventListener("click", addCounterEvent);
 
-// Add event listener
-myAdd.addEventListener("click", function () {
-  counter.innerHTML++;
-  setColor();
-});
-
-// Add event listener
-myReset.addEventListener("click", function () {
-  counter.innerHTML = 0;
-  setColor();
-});
-
-// Add event listener
-mySub.addEventListener("click", function () {
-  counter.innerHTML--;
-  setColor();
-});
-
-// Create a function to set the colors for the counter
-function setColor() {
-  if (counter.innerHTML > 0) {
-    counter.style.color = "green";
-  } else if (counter.innerHTML < 0) {
-    counter.style.color = "red";
-  } else {
-    counter.style.color = "black";
+function addCounterEvent(e) {
+  if (e.target.classList.contains("add")) {
+    counter.innerHTML++;
+  }
+  if (e.target.classList.contains("sub")) {
+    counter.innerHTML--;
+  }
+  if (e.target.classList.contains("reset")) {
+    counter.innerHTML = 0;
   }
 }
